16e92467ae7ea34b3c2c0424559f00bb92cf5857
Piotr Nowak <peterek99914@interia.pl>
2021-03-16 17:43:37 +0100

    Dodaj pliki projektów.

---
+++ Kartkówka2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kartkówka2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
